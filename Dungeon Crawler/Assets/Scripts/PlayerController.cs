d8c530d41b694028ade9301608c2f39ad3bc88b2
FootLettuce26 <caleb.weigt@cuw.edu>
2023-03-31 14:50:38 -0500

    Homework is finished
    Each exit takes resets the scene and you once you click an arrow, you can't click another one

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public GameObject northExit, westExit, southExit, eastExit;
     public float movementSpeed = 40.0f;
+    private bool keysActive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +20,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+       
+        if(Input.GetKeyDown(KeyCode.UpArrow) && (keysActive == true))
         {
             this.rb.AddForce(this.northExit.transform.position * movementSpeed); //accessing the position vector in transform
+            keysActive = false;
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) && (keysActive == true))
+        {
+            this.rb.AddForce(this.westExit.transform.position * movementSpeed); //accessing the position vector in transform
+            keysActive = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow) && (keysActive == true))
+        {
+            this.rb.AddForce(this.eastExit.transform.position * movementSpeed); //accessing the position vector in transform
+            keysActive = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow) && (keysActive == true))
+        {
+            this.rb.AddForce(this.southExit.transform.position * movementSpeed); //accessing the position vector in transform
+            keysActive = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Exits"))
         {
             MasterData.count++;
             SceneManager.LoadScene("DungeonRoom"); //asks it to load a scene 
         }
     }
 }
+
+
+
+
+
+//MasterData.count++;
+//SceneManager.LoadScene("DungeonRoom"); //asks it to load a scene 
